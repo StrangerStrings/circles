f8cb0f79f8b01f35e49056d01cdce6a5ccda6ff1
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2019-06-05 17:55:35 +0100

    Added color changing functionality

--- README.md
+++ README.md
@@ -26,3 +26,21 @@ q w e r t y u i o p
   s d f g h j k l
   x c v b n m  
 ```
+
+<br/>
+
+## Colors
+
+Number keys control the colors
+
+ctrl + number turns every shape to that color
+
+To change individual shapes color, <br/>
+Press a number whilst chosen shape's <br/>
+keys are held down or are on screen due to shift 
+
+Zero is back to white. ctrl + zero to reset all to white
+<br/><br/><br/>
+(There is a strange bug where when one or more letters are held down<br/>
+the number key directly above them does actually change their color. <br/>
+Try changing one shape's color at a time instead.)
--- script.js
+++ script.js
@@ -5,29 +5,29 @@ document.addEventListener('keydown', kiPres)
 
 function kiPres(e) {
 
-    // console.log(e.key)
+    if (e.repeat) return
 
+    console.log(e.key)
+
+
+    // remove light-up from shifted shape
     try {
-        document.getElementById('shape-shift-' + e.key).classList.remove('light-up')
+        document.getElementById('shape-' + e.key.toUpperCase()).classList.remove('light-up')
+    } catch{ }
+    
+
+    // add light-up to pressed key's shape
+    try {
+        document.getElementById('shape-' + e.key).classList.add('light-up')
     } catch{ }
 
-    if (e.shiftKey) {
-        console.log('shifted')
-        try {
-            document.getElementById('shape-shift-' + e.key.toLowerCase()).classList.add('light-up')
-        } catch{ }
-    }
-    else {
-        try {
-            document.getElementById('shape-' + e.key).classList.add('light-up')
-        } catch{ }
-    }
 
-    if (e.key == ' '){
-        var spaceDiv = document.getElementById('shape-space-'+spaceCount)
+    // space bar - random circle
+    if (e.key == ' ') {
+        var spaceDiv = document.getElementById('shape-space-' + spaceCount)
 
         let dimensions = 2 + Math.random() * 13
-        if( e.ctrlKey) dimensions *= 2
+        if (e.ctrlKey) dimensions *= 2
         spaceDiv.style.width = dimensions + 'vw'
         spaceDiv.style.height = dimensions + 'vw'
         spaceDiv.style.top = (Math.random() * 80) + 10 + '%'
@@ -36,6 +36,29 @@ function kiPres(e) {
         spaceDiv.classList.add('light-up')
     }
 
+
+    // numbers - color changing 
+    if (!Number.isNaN(parseInt(e.key))) {
+        console.log('change colors')
+        if (e.ctrlKey) {
+            var allDivs = $('div')
+            allDivs.removeClass(allColors)
+            allDivs.addClass('color-' + e.key)
+        }
+        else {
+            $('.light-up').each((index, elem) => {
+                var letter = $(elem).attr('class').split(' ')[0]
+                var divsToChange = $('.'+letter)
+                divsToChange.removeClass(allColors)
+                divsToChange.addClass('color-' + e.key)
+            })
+        }
+    }
+
+    function allColors(index, className) {
+        let matchedClasses = className.match(/(^|\s)color-\d+/g);
+        return (matchedClasses || []).join('');
+    };
 }
 
 
@@ -43,12 +66,12 @@ document.addEventListener('keyup', kiOff)
 
 function kiOff(e) {
     try {
-        document.getElementById('shape-' + e.key).classList.remove('light-up')
+        document.getElementById('shape-' + e.key.toLowerCase()).classList.remove('light-up')
     } catch{ }
 
-    if (e.key == ' '){
-        document.getElementById('shape-space-'+spaceCount).classList.remove('light-up')
-        spaceCount ++
+    if (e.key == ' ') {
+        document.getElementById('shape-space-' + spaceCount).classList.remove('light-up')
+        spaceCount++
         if (spaceCount == 9) spaceCount = 1
     }
 }
