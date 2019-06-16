ed8ebf9aa4762d8cd50faff1187402ebff67dfe6
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2019-06-16 09:14:02 +0100

    removed jquery dependency by using name not class

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 
 let spaceCount = 1
 
-document.addEventListener('keydown', kiPres)
-
-function kiPres(e) {
+document.addEventListener('keydown', function (e) {
 
     if (e.repeat) return
 
     console.log(e.key)
 
 
-    // remove light-up from shifted shape
+    // remove lit from shifted shape
     try {
-        document.getElementById('shape-' + e.key.toUpperCase()).classList.remove('light-up')
+        document.getElementById('shape-' + e.key.toUpperCase()).classList.remove('lit')
     } catch{ }
     
 
-    // add light-up to pressed key's shape
+    // add lit to pressed key's shape
     try {
-        document.getElementById('shape-' + e.key).classList.add('light-up')
+        document.getElementById('shape-' + e.key).classList.add('lit')
     } catch{ }
 
 
     // space bar - random circle
     if (e.key == ' ') {
-        var spaceDiv = document.getElementById('shape-space-' + spaceCount)
+        const spaceDiv = document.getElementById('shape-space-' + spaceCount)
 
         let dimensions = 2 + Math.random() * 13
         if (e.ctrlKey) dimensions *= 2
@@ -33,45 +31,37 @@ function kiPres(e) {
         spaceDiv.style.top = (Math.random() * 80) + 10 + '%'
         spaceDiv.style.left = (Math.random() * 80) + 10 + '%'
 
-        spaceDiv.classList.add('light-up')
+        spaceDiv.classList.add('lit')
     }
 
 
     // numbers - color changing 
-    if (!Number.isNaN(parseInt(e.key))) {
-        console.log('change colors')
-        if (e.ctrlKey) {
-            var allDivs = $('div')
-            allDivs.removeClass(allColors)
-            allDivs.addClass('color-' + e.key)
+    if (!isNaN(parseInt(e.key))) {
+
+        if (event.ctrlKey){
+            const allDivs = document.getElementsByTagName('div')
+            for (let i=0; i<allDivs.length; i++){
+                allDivs[i].setAttribute('name', 'color-'+event.key)
+            }
         }
-        else {
-            $('.light-up').each((index, elem) => {
-                var letter = $(elem).attr('class').split(' ')[0]
-                var divsToChange = $('.'+letter)
-                divsToChange.removeClass(allColors)
-                divsToChange.addClass('color-' + e.key)
-            })
+        const litDivs = document.getElementsByClassName('lit')
+        for (let i=0; i<litDivs.length; i++){
+            const letter = litDivs[i].classList[0]
+            document.getElementById('shape-' + letter.toLowerCase()).setAttribute('name', 'color-'+event.key)
+            document.getElementById('shape-' + letter.toUpperCase()).setAttribute('name', 'color-'+event.key)
         }
     }
-
-    function allColors(index, className) {
-        let matchedClasses = className.match(/(^|\s)color-\d+/g);
-        return (matchedClasses || []).join('');
-    };
-}
+})
 
 
-document.addEventListener('keyup', kiOff)
-
-function kiOff(e) {
+document.addEventListener('keyup', function (e) {
     try {
-        document.getElementById('shape-' + e.key.toLowerCase()).classList.remove('light-up')
+        document.getElementById('shape-' + e.key.toLowerCase()).classList.remove('lit')
     } catch{ }
 
     if (e.key == ' ') {
-        document.getElementById('shape-space-' + spaceCount).classList.remove('light-up')
+        document.getElementById('shape-space-' + spaceCount).classList.remove('lit')
         spaceCount++
         if (spaceCount == 9) spaceCount = 1
     }
-}
+})
